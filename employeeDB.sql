049cd5e50444bd398a3c3add1a9aaa10b92c47d9
elliefh <elliefu18@gmail.com>
2021-02-10 08:49:28 -0800

    setting up tables in mysql

---
+++ employeeDB.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  salary DECIMAL(3,2) NOT NULL,
+  department_id INTEGER(10) AUTO_INCREMENT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER(10),
+  manager_id INTEGER(10),
+  PRIMARY KEY (id)
+);
