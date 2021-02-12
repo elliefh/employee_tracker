b9add839767660ec8aeb11066a1e18e3219280fc
elliefh <elliefu18@gmail.com>
2021-02-12 14:48:03 -0800

    updated columns in role and employee tables

--- employeeDB.sql
+++ employeeDB.sql
@@ -13,15 +13,18 @@ CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(100) NOT NULL,
   salary DECIMAL(3,2) NOT NULL,
-  department_id INTEGER(10) AUTO_INCREMENT,
-  PRIMARY KEY (id)
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER(10),
+  role_id INT,
   manager_id INTEGER(10),
   PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
+
